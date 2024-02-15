51c91328a6a178b3918c34dd28649727662b59f8
Paul Ogolla <paulotieno2@gmail.com>
2024-02-15 17:42:27 +0300

    create types

---
+++ app/types.ts
@@ -0,0 +1,16 @@
+export type ResidentType = {
+  status: string,
+  image: string,
+  name: string,
+  id?: string,
+}
+
+export type ResidentsType = {
+  residents: ResidentType[]
+}
+
+export type Location = {
+  name: string;
+  type: string;
+  residents: ResidentsType;
+}
